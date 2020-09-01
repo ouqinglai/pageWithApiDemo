6aa2f894d404a960a0874d4cf36a3de860e02176
ouql5 <ouql5@meicloud.com>
2020-09-01 10:02:43 +0800

    test actions

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const core = require('@actions/core');
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    // const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${'ms'} milliseconds ...`);
+    const ms = core.getInput('milliseconds');
+    core.info(`Waiting ${ms} milliseconds ...`);
 
     console.log((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
