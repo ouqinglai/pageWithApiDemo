9f52ddab05367a462a1a08ee5ef8190fbb42acb2
ouql5 <ouql5@meicloud.com>
2020-09-01 10:57:28 +0800

    test action 10

--- .github/actions/test/index.js
+++ .github/actions/test/index.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+const core = require('@actions/core');
+
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    const ms = core.getInput('milliseconds');
+    core.info(`Waiting ${ms} milliseconds ...`);
+
+    console.log((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+
+    core.setOutput('time', new Date().toTimeString());
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
