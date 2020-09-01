cdfe22e067996f82fcb9f60ed12e2e71b2689361
ouql5 <ouql5@meicloud.com>
2020-09-01 11:06:47 +0800

    test action 11

--- test.js
+++ test.js
@@ -1,18 +1,11 @@
-const core = require('@actions/core');
 const fetch = require('node-fetch')
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
 
     let result = await new Promise(resolve => {
-      setTimeout(() => {
-        resolve({ a: 1 })
-      }, 5000)
-
       fetch('https://okex.me/api/swap/v3/instruments').then(res => res.json())
       .then(res => {
         resolve(res[0])
@@ -20,10 +13,7 @@ async function run() {
     })
 
     console.log(111, JSON.stringify(result, null, 2)); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.setOutput('time', new Date().toTimeString());
   } catch (error) {
-    core.setFailed(error.message);
   }
 }
 
