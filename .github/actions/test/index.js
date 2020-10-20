f356300d8ef8afc153214afa340f73492883acee
ouql5 <ouql5@meicloud.com>
2020-10-20 17:08:10 +0800

    test123

--- .github/actions/test/index.js
+++ .github/actions/test/index.js
@@ -1,27 +1,28 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const fetch = require('node-fetch')
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    // const ms = core.getInput('milliseconds');
+    // core.info(`Waiting ${ms} milliseconds ...`);
+console.log(888,github.event.inputs.location)
+    // let result = await new Promise(resolve => {
+    //   // setTimeout(() => {
+    //   //   resolve({ a: 1 })
+    //   // }, 1000)
 
-    let result = await new Promise(resolve => {
-      // setTimeout(() => {
-      //   resolve({ a: 1 })
-      // }, 1000)
+    //   fetch('https://api.github.com/repos/ouqinglai/pageWithApiDemo/issues/1').then(res => res.json())
+    //   .then(res => {
+    //     resolve({title: res.title})
+    //   })
+    // })
 
-      fetch('https://api.github.com/repos/ouqinglai/pageWithApiDemo/issues/1').then(res => res.json())
-      .then(res => {
-        resolve({title: res.title})
-      })
-    })
+    // console.log(111, JSON.stringify(result, null, 2)); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    console.log(111, JSON.stringify(result, null, 2)); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.setOutput('time', new Date().toTimeString());
+    // core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
